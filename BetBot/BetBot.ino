d71bad5501505c30033878d6477481d57abaa7ad
David Olsson <david.olsson@betssongroup.com>
2015-12-03 16:32:27 +0100

    Last minute updates

--- BetBot/BetBot.ino
+++ BetBot/BetBot.ino
@@ -9,7 +9,7 @@
 
 #define TRIGGER_PIN  2
 #define ECHO_PIN     6
-#define MAX_DISTANCE 200
+#define MAX_DISTANCE 1000
  
 // this might need to be tuned for different lighting conditions, surfaces, etc.
 #define QTR_THRESHOLD  1000 // microseconds
@@ -17,9 +17,11 @@
 // these might need to be tuned for different motor types
 #define REVERSE_SPEED     400 // 0 is stopped, 400 is full speed
 #define TURN_SPEED        200
-#define FORWARD_SPEED     200
-#define REVERSE_DURATION  100 // ms
-#define TURN_DURATION     200 // ms
+#define FORWARD_SPEED     300
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     400 // ms
+
+long randomNumber;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 ZumoBuzzer buzzer;
@@ -36,10 +38,13 @@ void waitForButtonAndCountDown()
   digitalWrite(LED, HIGH);
   button.waitForButton();
   digitalWrite(LED, LOW);
+  delay(3000);
 }
  
 void setup()
 {
+
+  randomSeed(42); 
   // uncomment if necessary to correct motor directions
   //motors.flipLeftMotor(true);
   //motors.flipRightMotor(true);
@@ -60,13 +65,8 @@ void loop()
     button.waitForRelease();
     waitForButtonAndCountDown();
   }
-  
-  //borderDetect();
-  /*if(borderDetect()) {
-    
-  } else {*/
-    sensorDetect(10);
-  //}
+ 
+  sensorDetect(70);
 }
 
 bool borderDetect()
@@ -77,7 +77,8 @@ bool borderDetect()
   {
     // if leftmost sensor detects line, reverse and turn to the right
     motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
+    randomNumber = random(100,400);
+    delay(randomNumber);
     motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
     delay(TURN_DURATION);
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
@@ -88,7 +89,8 @@ bool borderDetect()
   {
     // if rightmost sensor detects line, reverse and turn to the left
     motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
+    randomNumber = random(100,400);
+    delay(randomNumber);
     motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
     delay(TURN_DURATION);
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
@@ -105,14 +107,9 @@ bool borderDetect()
 
 bool sensorDetect(int attackDistance)
 {
-  //Serial.print(uS / US_ROUNDTRIP_CM);
   delay(50);
   int sensorDistance = sonar.ping();
   int sensorDistanceCM = sensorDistance / US_ROUNDTRIP_CM;
-  
-  Serial.print("Ping: ");
-  Serial.print(sensorDistance / US_ROUNDTRIP_CM);
-  Serial.println("cm");
 
   if (sensorDistanceCM > 0 && sensorDistanceCM < attackDistance) { 
     attack();
@@ -123,8 +120,8 @@ bool sensorDetect(int attackDistance)
 
 void attack() 
 {
-  int attackSpeed = 400;
-  Serial.print("Attack!");
+  int attackSpeed = 350;
+  //Serial.print("Attack!");
   motors.setSpeeds(attackSpeed, attackSpeed);
 }
 
